99be71b7048680be394942cd1257bb1070a19869
Tom-Lewis-Coder <64904837+Tom-Lewis-Coder@users.noreply.github.com>
2021-01-26 12:35:38 +0000

    Update limitStringLength.js

--- limitStringLength.js
+++ limitStringLength.js
@@ -1,4 +1,3 @@
 const solution = (string,limit) => {
-  let add = '...'
-  return string.length <= limit ? string : string.slice(0, limit).concat(add)
+  return string.length <= limit ? string : string.slice(0, limit).concat('...)
 }
