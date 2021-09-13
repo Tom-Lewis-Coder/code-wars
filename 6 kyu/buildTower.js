79ddb75635a5a5f29de5d720050f2b183f56c8ab
Tom-Lewis-Coder <64904837+Tom-Lewis-Coder@users.noreply.github.com>
2021-09-13 17:32:51 +0100

    Update buildTower.js

--- 6 kyu/buildTower.js
+++ 6 kyu/buildTower.js
@@ -35,7 +35,7 @@ and a tower of 6 floors looks like below
 Go challenge Build Tower Advanced once you have finished this :)
 */
 
-function towerBuilder(nFloors) {
+const towerBuilder = nFloors => {
     function arrFill(num) {
         arr = []
         for(let i = 1; i <= num; i++){
@@ -43,10 +43,10 @@ function towerBuilder(nFloors) {
         }
         return arr.map(x => '*')
     }
-    let strLength = (nFloors * 2) - 1
-    return Array.from(Array(nFloors).keys())
-  				.map((el,i) => el + (i+1))
-                .map(num => arrFill(num).toString().replace(/[,]/g, ''))
-                .map(s => s.padStart(s.length + Math.floor((strLength - s.length) / 2), ' ')
-                .padEnd(strLength, ' '))
-            }
+  let strLength = (nFloors * 2) - 1
+  return Array.from(Array(nFloors).keys())
+  				    .map((el,i) => el + (i+1))
+              .map(num => arrFill(num).toString().replace(/[,]/g, ''))
+              .map(s => s.padStart(s.length + Math.floor((strLength - s.length) / 2), ' ')
+              .padEnd(strLength, ' '))
+}
