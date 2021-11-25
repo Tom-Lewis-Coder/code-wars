29d3817fc21a5250b10c95e6c57e47831465a383
Tom-Lewis-Coder <tomlewis139@gmail.com>
2021-11-25 15:32:17 +0000

    add solution

---
+++ 6 kyu/twistedSum.js
@@ -0,0 +1,20 @@
+/*
+Find the sum of the digits of all the numbers from 1 to N (both ends included).
+
+Examples
+# N = 4
+1 + 2 + 3 + 4 = 10
+
+# N = 10
+1 + 2 + 3 + 4 + 5 + 6 + 7 + 8 + 9 + (1 + 0) = 46
+
+# N = 12
+1 + 2 + 3 + 4 + 5 + 6 + 7 + 8 + 9 + (1 + 0) + (1 + 1) + (1 + 2) = 51
+*/
+
+const twistedSum = n => {
+  return [...Array(n+1).keys()]
+                       .map(n => n.toString().split(''))
+                       .flat()
+                       .reduce((a,b) => Number(a)+Number(b))
+}
