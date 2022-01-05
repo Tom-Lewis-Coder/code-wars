18bc7ae3e900d11318d34f1d4fc01e1f374c221f
Tom-Lewis-Coder <tomlewis139@gmail.com>
2022-01-05 02:14:19 +0000

    add solution changes

--- 6 kyu/multiplicationTable.js
+++ 6 kyu/multiplicationTable.js
@@ -10,7 +10,6 @@ for given example, the return value should be: [[1,2,3],[2,4,6],[3,6,9]]
 */
 
 multiplicationTable = function(size) {
-  return [...Array(size).keys()]
-                        .map(n => [...Array(size).keys()].map((_,i) => i + 1))
-                        .map((arr, ind) => arr.map(n => n * [ind+1]))
+  return [...Array(size)].map(n => [...Array(size)].map((_,i) => i + 1))
+                         .map((arr, ind) => arr.map(n => n * [ind + 1]))
 }
