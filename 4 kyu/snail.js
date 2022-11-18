de1ba544e29d559de85b0671d61d079ec687fdb0
Tom-Lewis-Coder <tomlewis139@gmail.com>
2022-11-18 09:22:42 +0000

    add solution

--- 4 kyu/snail.js
+++ 4 kyu/snail.js
@@ -20,28 +20,28 @@ NOTE: The idea is not sort the elements from the lowest value to the highest; th
 NOTE 2: The 0x0 (empty matrix) is represented as en empty array inside an array [[]].
 */
 
-snail = function(array) {
-    let resultArr = []
-    let middleNum = array[Math.floor(array.length / 2)][Math.floor(array.length / 2)]
-  
-    const outerFunc = a => {
-      let res = []
-      let orderSeq = [...Array(a.length)].map((e, i) => i).concat([...Array(a.length - 2)].map((e, i) => i + 1).reverse())
-      orderSeq.map((n, i) => n == 0 ?
-        res.push(array[n].splice(0)) :
-        i < (a.length - 1) ? res.push(array[n].splice(a.length - 1, 1)) :
-        i === (a.length - 1) ? res.push(array[a.length - 1].splice(0).reverse()) :
-        res.push(array[n].splice(0, 1))
-      )
-      return resultArr.push(res.flat())
-    }
-  
-    while (array.filter(a => a.length).length > 1) {
-      array = array.filter(a => a.length)
-      outerFunc(array)
-    }
-  
-    return array.length < 2 ? array.flat() :
-      array.length % 2 == 0 ? resultArr.flat() :
-      resultArr.flat().concat(middleNum)
+const snail = array => {
+  let resultArr = []
+  let middleNum = array[Math.floor(array.length / 2)][Math.floor(array.length / 2)]
+
+  const outerFunc = a => {
+    let res = []
+    let orderSeq = [...Array(a.length)].map((e, i) => i).concat([...Array(a.length - 2)].map((e, i) => i + 1).reverse())
+    orderSeq.map((n, i) => n == 0 ?
+      res.push(a[n].splice(0)) :
+      i < (a.length - 1) ? res.push(a[n].splice(a.length - 1, 1)) :
+      i === (a.length - 1) ? res.push(a[a.length - 1].splice(0).reverse()) :
+      res.push(a[n].splice(0, 1))
+    )
+    return res.flat()
   }
+
+  while (array.filter(arr => arr.length).length > 1) {
+    array = array.filter(arr => arr.length)
+    resultArr.push(outerFunc(array))
+  }
+
+  return array.length < 2 ? array.flat() :
+    array.length % 2 == 0 ? resultArr.flat() :
+    resultArr.flat().concat(middleNum)
+}
