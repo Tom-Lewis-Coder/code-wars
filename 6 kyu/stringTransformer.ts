06cdb33db11ebc5225e6f994085cc00b98506dd9
Tom Lewis <tomlewis139@gmail.com>
2023-04-20 14:47:56 +0100

    add solution

---
+++ 6 kyu/stringTransformer.ts
@@ -0,0 +1,20 @@
+/*
+DESCRIPTION:
+Given a string, return a new string that has transformed based on the input:
+
+Change case of every character, ie. lower case to upper case, upper case to lower case.
+Reverse the order of words from the input.
+Note: You will have to handle multiple spaces, and leading/trailing spaces.
+
+For example:
+
+"Example Input" ==> "iNPUT eXAMPLE"
+You may assume the input only contain English alphabet and spaces.
+
+FUNDAMENTALS
+*/
+
+export function stringTransformer(str: string): string {
+  return str.split(' ').reverse().join(' ')
+            .split('').map((s: string) => s === s.toUpperCase() ? s.toLowerCase() : s.toUpperCase()).join('')
+}
