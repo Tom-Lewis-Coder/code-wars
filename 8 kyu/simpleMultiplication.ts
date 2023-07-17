a001b462704e519512079a7fa928e0ec6d2cb828
Tom Lewis <tomlewis139@gmail.com>
2023-07-17 16:27:31 +0100

    add solution

---
+++ 8 kyu/simpleMultiplication.ts
@@ -0,0 +1,11 @@
+/*
+DESCRIPTION:
+This kata is about multiplying a given number by eight if it is an even number and by nine otherwise.
+
+FUNDAMENTALS
+*/
+
+export function simpleMultiplication(num: number): number {
+    return num % 2 === 0 ? num * 8 : num * 9
+}
+
