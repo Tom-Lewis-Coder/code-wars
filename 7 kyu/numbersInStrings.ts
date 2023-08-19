3d558fba5de546edee1880e98fdb61619d4d9e1f
Tom Lewis <tomlewis139@gmail.com>
2023-08-19 17:58:17 +0100

    add solution

---
+++ 7 kyu/numbersInStrings.ts
@@ -0,0 +1,17 @@
+/*
+DESCRIPTION:
+In this Kata, you will be given a string that has lowercase letters and numbers. Your task is to compare the number groupings and return the largest number. Numbers will not have leading zeros.
+
+For example, solve("gh12cdy695m1") = 695, because this is the largest of all number groupings.
+
+Good luck!
+
+Please also try Simple remove duplicates
+
+STRINGSALGORITHMS
+*/
+
+export function solve(s: string) {
+  let numArr = s.match(/\d+/g)?.sort((a, b) => +b - +a)
+  return numArr && +numArr[0]
+}
