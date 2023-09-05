fd74568ee9700abe53071530531f98105553d7a1
Tom Lewis <tomlewis139@gmail.com>
2023-09-05 12:31:53 +0100

    codewars solutions

---
+++ PHP/8 kyu/fixmeReplaceAllDots.php
@@ -0,0 +1,19 @@
+/*
+DESCRIPTION:
+The code provided is supposed replace all the dots . in the specified String str with dashes -
+
+But it's not working properly.
+
+Task
+Fix the bug so we can all go home early.
+
+Notes
+String str will never be null.
+
+DEBUGGING
+*/
+
+<?php
+function replace_dots(string $str): string {
+  return str_replace('.', '-', $str);
+}
